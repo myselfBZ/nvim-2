6fae19eceb36dda04ab2eb6f7cd3766eef837cac
myselfBZ <boburforfun@gmail.com>
2026-07-05 17:10:21 +0500

    module

--- init.lua
+++ init.lua
@@ -1,67 +1,22 @@
-vim.opt.number = true
-vim.g.mapleader = " "
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-vim.opt.timeout = true
-vim.opt.timeoutlen = 300
-vim.opt.ttimeoutlen = 0
-vim.opt.relativenumber = true
-vim.opt.incsearch = true
-vim.opt.scrolloff = 8
-vim.opt.updatetime = 50
-vim.opt.termguicolors = true
-vim.opt.tabstop = 4
--- vim.opt.guicursor = ""
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.autoindent = true
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<C-f>", vim.cmd.wa)
-
+require("set")
+require("keymaps")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    {
-        'nvim-telescope/telescope.nvim', version = '*',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            -- optional but recommended
-            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-        }
-    },
-    {
-        'nvim-treesitter/nvim-treesitter',
-        lazy = false,
-        build = ':TSUpdate'
-    }
-}
-local opts = {};
 
-require("lazy").setup(plugins, opts)
+require("lazy").setup("plugins")
 
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
-
-
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-
-
-require('nvim-treesitter').install { 'rust', 'javascript', 'go'}
---
+++ lua/keymaps.lua
@@ -0,0 +1,3 @@
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<C-f>", vim.cmd.wa)
+vim.keymap.set('n', '<leader>b', "<cmd>q!<CR>")
