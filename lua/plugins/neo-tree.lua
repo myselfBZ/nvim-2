59f57983ae9593d83dfdfa371aaabef8884647cc
myselfBZ <boburforfun@gmail.com>
2026-07-07 22:37:41 +0500

    cool keymaps, fix lsp, no neo-tree only netrw

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,26 @@
-return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "MunifTanjim/nui.nvim",
-        "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, 
-
-    config = function()
-        vim.keymap.set('n', '<leader>pv', ':Neotree filesystem reveal left<CR>', {})
-    end
-}
+return {}
+-- trying to live without it
+-- return {
+-- 	"nvim-neo-tree/neo-tree.nvim",
+-- 	branch = "v3.x",
+-- 	dependencies = {
+-- 		"nvim-lua/plenary.nvim",
+-- 		"MunifTanjim/nui.nvim",
+-- 		"nvim-tree/nvim-web-devicons", -- optional, but recommended
+-- 	},
+-- 	lazy = false,
+--
+-- 	opt = {
+-- 		filesystem = {
+-- 			filtered_items = {
+-- 				visible = true,
+-- 				hide_dotfiles = false,
+-- 				hide_gitignored = true,
+-- 			},
+-- 		},
+-- 	},
+--
+-- 	config = function()
+-- 		vim.keymap.set("n", "<leader>pv", ":Neotree filesystem reveal left<CR>", {})
+-- 	end,
+-- }
